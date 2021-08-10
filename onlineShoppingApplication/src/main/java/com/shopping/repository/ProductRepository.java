7e3e4114e5607c3006d43df1d1b0ec6d24b3c8c8
RishavRana <rishavr10000@gmail.com>
2021-08-10 10:49:26 +0530

    thirdCommit

--- onlineShoppingApplication/src/main/java/com/shopping/repository/ProductRepository.java
+++ onlineShoppingApplication/src/main/java/com/shopping/repository/ProductRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.shopping.entities.Category;
 import com.shopping.entities.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-	
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+	public List<Product> findProductByCategory(Category category);
 }
